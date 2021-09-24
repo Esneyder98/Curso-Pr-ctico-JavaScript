b50ba70ca8d9aa769fbcbb602d833f5943917f10
Esneyder Saavedra <esneydersa@hotmail.com>
2021-09-24 17:30:36 -0500

    calculando area y perimetro desde el triangulo de la vista

--- figuras.js
+++ figuras.js
@@ -2,16 +2,13 @@
 console.group("Cuadrados");
 
 const perimetroCuadrado=lado => lado * 4;
-
-
 const areaCuadrado = lado => lado * lado;
-
 
 console.groupEnd();
 
 // Codigo del Triangulo
 console.group("Triangulo")
-const perimetroTriangulo = (lado1,lado2,base) => lado1+lado2+base
+const perimetroTriangulo = (lado1,lado2,base) =>lado1+lado2+base;
 
 const areaTriangulo =(base,altura)=> (base * altura)/2;
 console.groupEnd()
@@ -45,5 +42,29 @@ function calcularPerimetroCuadrado() {
     const value = input.value;
   
     const area = areaCuadrado(value);
+    alert(area);
+  }
+
+  function calcularPerimetroTriangulo() {
+    const inputLado1 = document.getElementById("InputLado1");
+    const valueLado1 = parseInt(inputLado1.value);
+
+    const inputLado2 = document.getElementById("InputLado2");
+    const valueLado2 = parseInt(inputLado2.value);
+
+    const inputBase = document.getElementById("InputBase");
+    const valueBase = parseInt(inputBase.value);
+  
+    const perimetro = perimetroTriangulo(valueLado1,valueLado2,valueBase);
+    alert(perimetro);
+  }
+  function calcularAreaTriangulo() {
+    const inputBase = document.getElementById("InputBase");
+    const valueBase = inputBase.value;
+
+    const inputAltura= document.getElementById("InputAltura");
+    const valueAltura = inputAltura.value;
+  
+    const area = areaTriangulo(valueBase,valueAltura);
     alert(area);
   }
