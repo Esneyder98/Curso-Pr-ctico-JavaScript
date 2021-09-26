8621e581abae674edc4e04a75ce78aab3c7b16f4
Esneyder Saavedra <esneydersa@hotmail.com>
2021-09-26 08:40:28 -0500

    Calculado la mediana

--- Promedio_mediana_moda/mediana.js
+++ Promedio_mediana_moda/mediana.js
@@ -10,24 +10,33 @@ const calcularMediaAritmetica=(lista=0)=>{
         return suma/lista.length
     }
 }
-const lista=[100,200,500,4000000]
-
-
-const mitadLista = parseInt(lista.length/2);
-
-function esPar(numero){
+const lista=[100,500,4000000,200]
+const esPar = numero=>{
     if (numero % 2 === 0){
         return true;
     }else{
         return false;
     }
 }
-let mediana;
-if(esPar(lista.length)){
-    const elemento1 = lista[mitadLista-1];
-    const elemento2 = lista[mitadLista];
-    const promedioElementos = calcularMediaAritmetica([elemento1,elemento2]);
-    mediana = promedioElementos;
-}else{
-    mediana=lista[mitadLista]
+const calcularMediana=(list)=>{
+    if(list.length >=1){
+        let ordenada = list.sort(
+            function (elementoA, elementoB) {
+              return elementoA - elementoB;
+            });
+        const mitadLista = parseInt(ordenada.length/2);
+
+        let mediana;
+        if(esPar(ordenada.length)){
+            const elemento1 = ordenada[mitadLista-1];
+            const elemento2 = ordenada[mitadLista];
+            const promedioElementos = calcularMediaAritmetica([elemento1,elemento2]);
+            return mediana = promedioElementos;
+        }else{
+            return mediana = lista[mitadLista]
+        }
+        
+    }else{
+        console.log("Valor no valido");
+    }
 }
